6b04c070d8490a7189e5443cf99b7980e3374c4f
Pete Gadomski <pete.gadomski@gmail.com>
2016-03-03 10:58:18 -0700

    Add cpd formula

---
+++ Formula/cpd.rb
@@ -0,0 +1,25 @@
+class Cpd < Formula
+  desc "Coherent Point Drift: point set registration"
+  homepage "https://gadomski.github.io/cpd"
+  url "https://github.com/gadomski/cpd/archive/v0.3.0.tar.gz"
+  sha256 ""
+  head "https://github.com/gadomski/cpd.git"
+
+  depends_on "cmake" => :build
+  depends_on "fgt"
+
+  def install
+    system "cmake", ".", "-DBUILD_SHARED_LIBS=ON", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write("find_package(Cpd REQUIRED)\n
+                                      add_executable(test main.cpp)\n
+                                      target_link_libraries(test PRIVATE Cpd::Library-C++)")
+    (testpath/"main.cpp").write("#include <cpd/rigid.hpp>\nint main(int argc, char** argv) {}")
+    system "cmake", "."
+    system "make"
+    system "./test"
+  end
+end
