5ab1f0c0b1b63c1afd0334ac57732c1a4ab4524f
Pete Gadomski <pete.gadomski@gmail.com>
2016-11-08 15:58:30 -0700

    Bump cpd version to v0.4.1

--- Formula/cpd.rb
+++ Formula/cpd.rb
@@ -1,15 +1,15 @@
 class Cpd < Formula
   desc "Coherent Point Drift: point set registration"
   homepage "https://gadomski.github.io/cpd"
-  url "https://github.com/gadomski/cpd/archive/v0.3.3.tar.gz"
-  sha256 "2507851aa8096265e842551a90c7658d19736ec6fdf2fe496491c93b760253ba"
+  url "https://github.com/gadomski/cpd/archive/v0.4.1.tar.gz"
+  sha256 "5be3c16261cdd693b7f177f27a6ebdc362018f56f7344c73902341945c3daf9f"
   head "https://github.com/gadomski/cpd.git"
 
   depends_on "cmake" => :build
   depends_on "fgt"
 
   def install
-    system "cmake", ".", "-DBUILD_SHARED_LIBS=ON", *std_cmake_args
+    system "cmake", ".", "-DBUILD_SHARED_LIBS=ON", "-DWITH_DOCS=OFF", "-DWITH_STRICT_WARNINGS=OFF", "-DWITH_TESTS=OFF", *std_cmake_args
     system "make", "install"
   end
 
@@ -17,7 +17,7 @@ class Cpd < Formula
     (testpath/"CMakeLists.txt").write("find_package(Cpd REQUIRED)\n
                                       add_executable(test main.cpp)\n
                                       target_link_libraries(test PRIVATE Cpd::Library-C++)")
-    (testpath/"main.cpp").write("#include <cpd/rigid.hpp>\nint main(int argc, char** argv) {}")
+    (testpath/"main.cpp").write("#include <cpd/version.hpp>\n#include <cstdio>\nint main(int argc, char** argv) { printf(\"%s\\n\", cpd::version()); return 0; }")
     system "cmake", "."
     system "make"
     system "./test"
