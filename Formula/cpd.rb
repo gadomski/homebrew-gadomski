8bd1e36109e1b9a1544747b3fa86d6a70591faa0
Pete Gadomski <pete.gadomski@gmail.com>
2016-11-28 08:16:40 -0700

    [cpd] v0.4.3

--- Formula/cpd.rb
+++ Formula/cpd.rb
@@ -1,15 +1,22 @@
 class Cpd < Formula
   desc "Coherent Point Drift: point set registration"
   homepage "https://gadomski.github.io/cpd"
-  url "https://github.com/gadomski/cpd/archive/v0.4.2.tar.gz"
-  sha256 "498b210e6b520632f06a86c136fe06c304ffef79e89b2abd8f8cc1fc75fb2b30"
+  url "https://github.com/gadomski/cpd/archive/v0.4.3.tar.gz"
+  sha256 "e2043420690e6de4a530745d7514a06719c4c267bd20727ef1ae13fee2bbb017"
   head "https://github.com/gadomski/cpd.git"
 
   depends_on "cmake" => :build
-  depends_on "fgt"
+  depends_on "fgt" => :recommended
 
   def install
-    system "cmake", ".", "-DBUILD_SHARED_LIBS=ON", "-DWITH_DOCS=OFF", "-DWITH_STRICT_WARNINGS=OFF", "-DWITH_TESTS=OFF", *std_cmake_args
+    args = ["-DWITH_DOCS=OFF", "-DWITH_STRICT_WARNINGS=OFF", "-DWITH_TESTS=OFF"]
+    if build.with? "fgt"
+      args << "-DWITH_FGT=ON"
+    else
+      args << "-DWITH_FGT=OFF"
+    end
+    args += std_cmake_args
+    system "cmake", ".", *args
     system "make", "install"
   end
 
