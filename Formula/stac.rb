38adad5fd4401e8ee1c6190661acaa6af078f587
Pete Gadomski <pete.gadomski@gmail.com>
2024-04-22 07:54:10 -0600

    feat: update stac

--- Formula/stac.rb
+++ Formula/stac.rb
@@ -1,11 +1,12 @@
 class Stac < Formula
   desc "Command line interface for working with STAC data and servers, written in Rust"
-  homepage "https://gtihub.com/gadomski/stac-rs"
-  url "https://github.com/gadomski/stac-rs/archive/refs/tags/stac-cli-v0.0.3.tar.gz"
-  sha256 "c1561c2c33fd967287e87e1e27240b2342541bc94ba275bd41d136c10dfb0ca0"
+  homepage "https://github.com/stac-utils/stac-rs"
+  url "https://github.com/stac-utils/stac-rs/archive/refs/tags/stac-cli-v0.0.7.tar.gz"
+  sha256 "e10336433d96d6674dbc066652cbc4dfe0d3b040c601e98729e451f6092e1d60"
   license "MIT OR Apache-2.0"
 
   depends_on "rust" => :build
+  depends_on "gdal"
 
   def install
     system "cargo", "install", "--locked", "--root", prefix, "--path", "stac-cli"
